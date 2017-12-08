e1ac7e96bd09153c5d70b1eb8221703aad2772b8
Dikshant Patodia <dikshant.patodia@redlooptech.com>
2017-12-08 15:30:14 +0530

    5 x 5 tictactoe game

--- README.md
+++ README.md
@@ -1,1 +1,109 @@
-# tictactoe
+# Tic-Tac-Toe
+
+A React implementation of Tic Tac Toe.
+
+## How to get started ?
+In command line, write the following commands:
+```sh
+npm install -g create-react-app
+
+create-react-app my-app
+cd my-app/
+npm start
+```
+
+Then open [http://localhost:3000/](http://localhost:3000/) to see your app.<br>
+When you’re ready to deploy to production, create a bundle with `npm run build`.
+
+### Get Started Immediately
+
+You **don’t** need to install or configure tools like Webpack or Babel.<br>
+They are preconfigured and hidden so that you can focus on the code.
+
+**You’ll need to have Node >= 6 on your machine**.
+
+### Creating an App
+
+To create a new app, run:
+
+```sh
+create-react-app tic-tac-toe
+cd tic-tac-toe
+```
+
+It will create a directory called `tic-tac-toe` inside the current folder.<br>
+Inside that directory, it will generate the initial project structure and install the transitive dependencies:
+
+```
+tic-tac-toe
+├── README.md
+├── node_modules
+├── package.json
+├── .gitignore
+├── public
+│   └── favicon.ico
+│   └── index.html
+│   └── manifest.json
+└── src
+    └── App.css
+    └── App.js
+    └── App.test.js
+    └── index.css
+    └── index.js
+    └── logo.svg
+    └── registerServiceWorker.js
+```
+
+Probably you can `delete` all the files in the `./src` folder and add a new file called `index.js` and also create a folder for styles where you can write the styles for the `App`.
+
+Since, the styles have been written in `scss`, so you also need to have a compiler to process it to `css`.
+For doing that, you can also install `sass-loader` using npm, but it requires a bit of configuration. So, to keep it simple install [Sass compiler](http://sass-lang.com/install).
+
+Command for compiling the `scss` file to `css`:
+```sh
+cd assets
+sass --watch style.scss:style.css
+```
+
+Kindly, refer the code for better understanding of the `files` and `codes`
+
+After doing all the above, the folder structure will look like:
+
+```
+tic-tac-toe
+├── README.md
+├── node_modules
+├── package.json
+├──  package-lock.json
+├── public
+│   └── favicon.png
+│   └── index.html
+│   └── manifest.json
+└── src
+    └── assets
+        └── style.css
+        └── style.css.map
+        └── style.scss
+    └── index.js
+```
+
+### `npm start`
+
+Runs the app in development mode.
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will automatically reload if you make changes to the code.<br>
+You will see the build errors and lint warnings in the console.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+Your app is ready to be deployed.
+
+##### Author
+Dikshant Patodia. Find me [here](http://dikshantpatodia.in).
+
+
+
---
+++ src/index.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/style.css';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+ 
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(25).fill(null),
+      xIsNext: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState ({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const title = 'Tic Tac Toe';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div>
+        <div className="title">{title}</div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare(10)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(11)}
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+          {this.renderSquare(14)}
+          {this.renderSquare(15)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(16)}
+          {this.renderSquare(17)}
+          {this.renderSquare(18)}
+          {this.renderSquare(19)}
+          {this.renderSquare(20)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(21)}
+          {this.renderSquare(22)}
+          {this.renderSquare(23)}
+          {this.renderSquare(24)}
+          {this.renderSquare(25)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class TicTacToe extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25],
+
+    [1, 6, 11, 16, 21],
+    [2, 7, 12, 17, 22],
+    [3, 8, 13, 18, 23],
+    [4, 9, 14, 19, 24],
+    [5, 10, 15, 20, 25],
+
+    [1, 7, 13, 19, 25],
+    [5, 9, 13, 17, 21]
+  ];
+  for (let count = 0; count < lines.length; count++) {
+    const [a, b, c, d, e] = lines[count];
+    /*
+    console.log("squares[a]: " + squares[a]);
+    */
+    if (squares[a] === squares[b] &&
+        squares[a] === squares[c] &&
+        squares[a] === squares[d] &&
+        squares[a] === squares[e]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+ReactDOM.render(<TicTacToe />, document.getElementById('root'));
